13f2ddcb2b644e1acbbc3cf74e23c59dcfa13698
Gokulnathz <gokulnaths1992@gmail.com>
2024-07-19 12:19:04 +0530

    car world

---
+++ script.js
@@ -0,0 +1,10 @@
+function store()
+{
+    // car={
+    //     carBrand:brand.value,
+    //     carPrice:price.value,
+    //     carKey:key.value
+    // }
+    console.log(document.getElementById('brand'));
+
+}
